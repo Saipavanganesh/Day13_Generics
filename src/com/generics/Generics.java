43d1d3223bdd9f324b92c4e76176ab58802a5951
Saipavanganesh <pavanganeshms@gmail.com>
2023-01-13 02:56:28 +0530

    first commit

---
+++ src/com/generics/Generics.java
@@ -0,0 +1,46 @@
+package com.generics;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class MyGeneric<T> {
+      T a1,a2,a3;
+    public MyGeneric(T a1, T a2, T a3) {
+        this.a1 = a1;
+        this.a2 = a2;
+        this.a3 = a3;
+    }
+   public static <T extends Comparable> void max(T a1, T a2, T a3){
+        T maxElement;
+       if (a1.compareTo(a2) > 0){
+           if(a1.compareTo(a3) > 0){
+               System.out.println("Maximum is " + a1);
+           }
+           else{
+               System.out.println("Maximum is " + a2);
+           }
+       }
+       else{
+           if(a2.compareTo(a3) > 0){
+               System.out.println("Maximum is " + a2);
+           }
+           else{
+               System.out.println("Maximum is " + a3);
+           }
+       }
+    }
+}
+public class Generics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num1 = sc.nextInt();
+        System.out.println("Enter a number");
+        int num2 = sc.nextInt();
+        System.out.println("Enter a number");
+        int num3 = sc.nextInt();
+        MyGeneric myGeneric = new MyGeneric(num1, num2, num3);
+        MyGeneric.max(num1, num2, num3);
+    }
+}
