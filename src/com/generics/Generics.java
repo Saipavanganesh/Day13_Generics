f6ff93b5bce121c47836f9809d04d4760d8e38a2
Saipavanganesh <pavanganeshms@gmail.com>
2023-01-18 17:26:40 +0530

    more Parameters

--- src/com/generics/Generics.java
+++ src/com/generics/Generics.java
@@ -6,41 +6,46 @@ import java.util.Scanner;
 
 public class Generics {
     public static void main(String[] args) {
+        MyGeneric myGeneric;
         Scanner sc = new Scanner(System.in);
         System.out.println("1.Max Int\n2.Max Float\n3.Max String\n4. Exit");
         int option = sc.nextInt();
+        int n;
         switch(option){
             case 1:
-                System.out.println("Enter a number");
-                int num1 = sc.nextInt();
-                System.out.println("Enter a number");
-                int num2 = sc.nextInt();
-                System.out.println("Enter a number");
-                int num3 = sc.nextInt();
-                MyGeneric myGenericInt = new MyGeneric(num1, num2, num3);
-                myGenericInt.max(num1, num2, num3);
+                System.out.println("Enter the no.of Integers you want to check\n");
+                n = sc.nextInt();
+                Integer[] arrInt = new Integer[n];
+                for(int i = 0; i < arrInt.length; i++) {
+                    System.out.println("Enter the values");
+                    arrInt[i] = sc.nextInt();
+                }
+                myGeneric = new MyGeneric(arrInt);
+                System.out.println("Max integer is " + myGeneric.findMaxElement());
                 break;
 
             case 2:
-                System.out.println("Enter a number");
-                float float1 = sc.nextFloat();
-                System.out.println("Enter a number");
-                float float2 = sc.nextFloat();
-                System.out.println("Enter a number");
-                float float3 = sc.nextFloat();
-                MyGeneric myGenericFloat = new MyGeneric(float1, float2, float3);
-                myGenericFloat.max(float1, float2, float3);
+                System.out.println("Enter the no.of Integers you want to check\n");
+                n = sc.nextInt();
+                Float[] arrFloat = new Float[n];
+                for(int i = 0; i < arrFloat.length; i++) {
+                    System.out.println("Enter the values");
+                    arrFloat[i] = sc.nextFloat();
+                }
+                myGeneric = new MyGeneric(arrFloat);
+                System.out.println("Max floating number is " + myGeneric.findMaxElement());
                 break;
 
             case 3:
-                System.out.println("Enter a word");
-                String str1 = sc.next();
-                System.out.println("Enter a word");
-                String str2 = sc.next();
-                System.out.println("Enter a word");
-                String str3 = sc.next();
-                MyGeneric myGenericStr = new MyGeneric(str1, str2,str3);
-                myGenericStr.max(str1, str2,str3);
+                System.out.println("Enter the no.of Integers you want to check\n");
+                n = sc.nextInt();
+                String[] arrStr = new String[n];
+                for(int i = 0; i < arrStr.length; i++) {
+                    System.out.println("Enter the values");
+                    arrStr[i] = sc.nextLine();
+                }
+                myGeneric = new MyGeneric(arrStr);
+                System.out.println("Max String is " + myGeneric.findMaxElement());
                 break;
 
             case 4:
--- src/com/generics/MyGeneric.java
+++ src/com/generics/MyGeneric.java
@@ -1,29 +1,14 @@
 package com.generics;
 
-public class MyGeneric <T> {
-    T a1,a2,a3;
-    public MyGeneric(T a1, T a2, T a3) {
-        this.a1 = a1;
-        this.a2 = a2;
-        this.a3 = a3;
+import java.util.Arrays;
+
+public class MyGeneric <T extends Comparable> {
+    T[] array;
+    public MyGeneric(T array[]){
+        this.array = array;
     }
-    public static <T extends Comparable> void max(T a1, T a2, T a3){
-        T maxElement;
-        if (a1.compareTo(a2) > 0){
-            if(a1.compareTo(a3) > 0){
-                System.out.println("Maximum is " + a1);
-            }
-            else{
-                System.out.println("Maximum is " + a2);
-            }
-        }
-        else{
-            if(a2.compareTo(a3) > 0){
-                System.out.println("Maximum is " + a2);
-            }
-            else{
-                System.out.println("Maximum is " + a3);
-            }
-        }
+    public T findMaxElement(){
+        Arrays.sort(array);
+        return array[array.length-1];
     }
 }
